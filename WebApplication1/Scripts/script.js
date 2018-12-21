7ac77cf35bd24b2a3987a7568432fa2cb2ac529c
Joel Carvalho <a12607@alunos.ipca.pt>
2018-12-21 14:10:05 +0000

    Show/Hide dropdowns in InformacoesUteis/Create

---
+++ WebApplication1/Scripts/script.js
@@ -0,0 +1,25 @@
+﻿// Mostrar ou esconder form da checkbox escolhida
+$(document).ready(function () {
+    $('#userRadio').click(function () {
+        $('.form-group-checkbox-postalCode').hide();
+        $('.form-group-checkbox-user').show();
+    });
+    $('#userPostalCode').click(function () {
+        $('.form-group-checkbox-user').hide();
+        $('.form-group-checkbox-postalCode').show();
+    });
+});
+
+// Acrescentar como default campo vazio na dropdown IdCodigoPostal (InformacoesUteis/Create)
+$(document).ready(function () {
+    var new_empty = new Option("", "", false, true);
+    $(new_empty).html("");
+    $("#IdCodigoPostal").append(new_empty);
+});
+
+// Acrescentar como default campo vazio na dropdown Destinatario (InformacoesUteis/Create)
+$(document).ready(function () {
+    var new_empty = new Option("", "", false, true);
+    $(new_empty).html("");
+    $("#Destinatario").append(new_empty);
+});
